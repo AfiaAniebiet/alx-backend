410fafa4e8b12668ec7cae909198ebd4b4da4d29
AfiaAniebiet <aniebietafia87@gmail.com>
2023-05-17 10:14:09 -0700

    [100-Advanced] LFU Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
-
-"""Create a class LFUCache that inherits
-from BaseCaching and is a caching system
-Must assign to the dictionary self.cache_data
-the item value for the key key.If key or item
-is None, this method should not do anything.If the
-number of items in self.cache_data is higher that
-BaseCaching.MAX_ITEMS:
-you must discard the least frequency used item (LFU algorithm)
-if you find more than 1 item to discard, you must
-use the LRU algorithm to discard only the least recently used
-you must print DISCARD: with the key discarded and
-following by a new line"""
+""" Python caching systems """
 
 from base_caching import BaseCaching
 
