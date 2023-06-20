6713305a568d07c34e2918c210397a257e1904e8
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-20 18:17:03 +0000

    Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,23 +1,24 @@
-import redis from 'redis';
+import { createClient, print } from 'redis'
 
-const client = redis.createClient();
+const client = createClient();
 
-client.on('error', (error) => {
-  console.log('Redis client not connected to the server: ', error);
+const redisConnect = () => {
+  client.on('error', err => console.log('Redis client not connected to the server: ', err));
+
+  client.on('connect', () => console.log('Redis client connected to the server'))
+}
+redisConnect();
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (error, resp) => {
+    print(`Output: ${resp}`);
+  });
 }
 
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-}
-
-const setNewSchool = function(schoolName, value) {
-  client.set(schoolName, value, redis.print);
-}
-
-const displaySchoolValue = function(schoolName) {
+function displaySchoolValue(schoolName) {
   client.get(schoolName, (err, response) => {
     console.log(response);
-  }
+  });
 }
 
 displaySchoolValue('Holberton');
